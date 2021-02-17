aafd513faef2960bb13845af59efde095f505972
viktor-lima <joaolimak10@outlook.com>
2021-02-17 17:08:07 -0300

    Declared lambda espression

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 import entities.Product;
 
@@ -18,7 +19,10 @@ public class Main {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::nonStaticProdutPredicate);
+		Predicate<Product> pred = p -> p.getPrice() >= 100;
+		
+		
+		list.removeIf(pred);
 		
 		
 		for (Product p : list) {
